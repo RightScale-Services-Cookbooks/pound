4ef3f59bc008deba83009458a744ee5ef711f677
Richard Shade <rshade@rightscale.com>
2013-10-22 19:23:20 -0500

    adding pound package

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,7 @@ license          'Apache 2.0'
 description      'Installs/Configures pound'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+
+
+recipe "pound::default", "installs and configures pound"
--- recipes/default.rb
+++ recipes/default.rb
@@ -16,3 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+package "pound" do
+  action :install
+end
+
