130e54c0efbeba65078682e63566d8ae5ed65b7b
Richard Shade <rshade@rightscale.com>
2013-10-23 14:58:12 -0500

    adding ssl attributes

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,27 @@ maintainer_email 'premium@rightscale.com'
 license          'Apache 2.0'
 description      'Installs/Configures pound'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 recipe "pound::default", "installs and configures pound"
 recipe "pound::setup_http", "sets up http frontend"
 recipe "pound::setup_https", "sets up https frontend"
+
+attribute "pound/ssl/chain",
+  :display_name => "Pound SSL Chainfile",
+  :description => "Pound SSL Chainfile",
+  :required => "required",
+  :recipes => [ "pound::setup_https" ]
+
+attribute "pound/ssl/key",
+  :display_name => "Pound SSL Key",
+  :description => "Pound SSL Key",
+  :required => "required",
+  :recipes => [ "pound::setup_https" ]
+
+attribute "pound/ssl/cert",
+  :display_name => "Pound SSL Certificate",
+  :description => "Pound SSL Certificate",
+  :required => "required",
+  :recipes => [ "pound::setup_https" ]
+
